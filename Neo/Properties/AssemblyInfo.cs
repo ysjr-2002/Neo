3f333fcf0471a151a116ba8ee20d87be4ab35eea
ysj <ysjr-2002@163.com>
2016-11-18 23:46:22 +0800

    ok

--- Neo/Properties/AssemblyInfo.cs
+++ Neo/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Neo")]
+[assembly: AssemblyTitle("Obria")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Neo")]
+[assembly: AssemblyCompany("Obria")]
+[assembly: AssemblyProduct("Vistor")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
